754843e5524f5cb63313ea4cd068ae2f94276218
Bart Thate <bthate@dds.nl>
2023-12-24 18:52:50 +0100

    timer is back again

--- objx/run.py
+++ objx/run.py
@@ -6,13 +6,9 @@
 "main"
 
 
-import getpass
 import inspect
 import os
-import pwd
 import readline
-import sys
-import termios
 import time
 
 
@@ -32,10 +28,11 @@ from .thread  import launch
 from .utility import forever
 
 
-Cfg = Default()
-
-
-Storage.wd = Cfg.wd
+def __dir__():
+    return (
+        'cmnd',
+        'scan'
+    )
 
 
 def cmnd(txt):
@@ -44,36 +41,6 @@ def cmnd(txt):
     Command.handle(evn)
     evn.wait()
     return evn
-
-
-def daemon(pidfile, verbose=False):
-    pid = os.fork()
-    if pid != 0:
-        os._exit(0)
-    os.setsid()
-    pid2 = os.fork()
-    if pid2 != 0:
-        os._exit(0)
-    if not verbose:
-        with open('/dev/null', 'r', encoding="utf-8") as sis:
-            os.dup2(sis.fileno(), sys.stdin.fileno())
-        with open('/dev/null', 'a+', encoding="utf-8") as sos:
-            os.dup2(sos.fileno(), sys.stdout.fileno())
-        with open('/dev/null', 'a+', encoding="utf-8") as ses:
-            os.dup2(ses.fileno(), sys.stderr.fileno())
-    os.umask(0)
-    os.chdir("/")
-    if os.path.exists(pidfile):
-        os.unlink(pidfile)
-    cdir(os.path.dirname(pidfile))
-    with open(pidfile, "w", encoding="utf-8") as fds:
-        fds.write(str(os.getpid()))
-
-
-def privileges(username):
-    pwnam = pwd.getpwnam(username)
-    os.setgid(pwnam.pw_gid)
-    os.setuid(pwnam.pw_uid)
 
 
 def scan(pkg, modstr, initer=False) -> []:
@@ -97,34 +64,3 @@ def scan(pkg, modstr, initer=False) -> []:
             module._thr = launch(module.init, name=f"init {modname}")
         mods.append(module)
     return mods
-
-
-def wrap(func) -> None:
-    old2 = None
-    try:
-        old2 = termios.tcgetattr(sys.stdin.fileno())
-    except termios.error:
-        pass
-    try:
-        func()
-    except (KeyboardInterrupt, EOFError):
-        print("")
-    finally:
-        if old2:
-            termios.tcsetattr(sys.stdin.fileno(), termios.TCSADRAIN, old2)
-
-
-def main():
-    try:
-        import objx.mods as modules
-    except ModuleNotFoundError:
-        modules = None
-    Storage.skel()
-    parse_command(Cfg, " ".join(sys.argv[1:]))
-    if not Cfg.mod:
-        Cfg.mod = ",".join(modules.__dir__())
-    if "v" in Cfg.opts:
-        dte = time.ctime(time.time()).replace("  ", " ")
-        debug(f"{Cfg.name.upper()} started {Cfg.opts.upper()} started {dte}")
-    scan(modules, Cfg.mod)
-    cmnd(Cfg.otxt)
