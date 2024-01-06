6195939c344b8e6157cde3ba9a432074c14ddf83
Bart Thate <bthate@dds.nl>
2024-01-06 19:29:03 +0100

    add init

---
+++ mods/__init__.py
@@ -0,0 +1,25 @@
+# This file is placed in the Public Domain.
+#
+#
+
+
+"local"
+
+
+
+from . import fnd, mbx, mdl, req, rst, udp, wsd
+
+
+def __dir__():
+    return (
+        'fnd',
+        'mbx',
+        'mdl',
+        'req',
+        'rst',
+        'udp',
+        'wsd'
+    )
+
+
+__all__ = __dir__()
