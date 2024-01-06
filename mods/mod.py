ec63a7907fc10e8a6e3050d65614fb13b426769b
Bart Thate <bthate@dds.nl>
2024-01-06 19:45:22 +0100

    add mod command

---
+++ mods/mod.py
@@ -0,0 +1,21 @@
+# This file is placed in the Public Domain.
+#
+# pylint: disable=C,R,E0402
+
+
+"available modules"
+
+
+import os
+import sys
+
+
+from . import __dir__
+
+
+def mod(event):
+    try:
+        import objmod
+        event.reply(",".join(sorted(__dir__() + objmod.__dir__())))
+    except ModuleNotFoundError:
+        event.reply(",".join(__dir__()))
